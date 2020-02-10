52bc848e4e722c1bc90e25c3d14cb4d23a9446cb
Luis Manuel Brito <lsbrt4@gmail.com>
2020-02-10 12:37:49 -0400

    Agrego controlador y el mock para el store

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const response = require('../../../network/response');
+const Controller = require('./controller')
 
 const router = express.Router();
 
 router.get('/', function(req, res){
 /*     res.send('todo funciona'); */
-    response.success(req, res, 'Todo correcto', 200)
+    const lista =  Controller.list();
+    response.success(req, res, lista, 200)
 });
 
 module.exports = router;
