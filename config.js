f7755e7e9debb9cc34dc398c554c47692308a5b6
Luis Manuel Brito <lsbrt4@gmail.com>
2020-02-23 09:56:02 -0400

    Configuracion de now

--- config.js
+++ config.js
@@ -7,20 +7,30 @@ module.exports = {
         port: process.env.POST_PORT || 3003
     },
     jwt: {
-        secret: process.env.JWT_SECRET || 'notasecret'
+        secret: process.env.JWT_SECRET || ''
     },
+    /*     mysql: {
+            host: process.env.MYSQL_HOST || '127.0.0.1',
+
+            user: process.env.MYSQL_USER || 'lbrito',
+
+            password: process.env.MYSQL_PASS || 'ThuReal.Dulc3',
+
+            database: process.env.MYSQL_DB || 'movenu_db',
+
+        }, */
     mysql: {
-        host: process.env.MYSQL_HOST || '127.0.0.1',
+        host: process.env.MYSQL_HOST || '',
 
-        user: process.env.MYSQL_USER || 'lbrito',
+        user: process.env.MYSQL_USER || '',
 
-        password: process.env.MYSQL_PASS || 'ThuReal.Dulc3',
+        password: process.env.MYSQL_PASS || '',
 
-        database: process.env.MYSQL_DB || 'movenu_db',
+        database: process.env.MYSQL_DB || '',
 
     },
     mysqlService: {
-        host: process.env.MYSQL_SRV_HOST || '127.0.0.1',
+        host: process.env.MYSQL_SRV_HOST || '',
         port: process.env.MYSQL_SRV_PORT || 3001,
     }
 }
