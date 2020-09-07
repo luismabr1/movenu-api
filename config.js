3517f8ac269d606f98e787cf2186cb55aaf95513
Luis Manuel Brito <lsbrt4@gmail.com>
2020-09-07 16:54:31 -0400

    Esto es un cambio en desarrollo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     remoteDB: process.env.REMOTE_DB || false,
     api: {
-        port: process.env.API_PORT || 3002
+        port: process.env.API_PORT || 3001
     },
     post: {
         port: process.env.POST_PORT || 3003
@@ -12,49 +12,47 @@ module.exports = {
     jwt: {
         secret: process.env.JWT_SECRET || 'nosecret'
     },
-    /*     mysql: {
-            host: process.env.MYSQL_HOST || '127.0.0.1',
+    mysql: {
+        host: process.env.MYSQL_HOST || 'remotemysql.com',
 
-            user: process.env.MYSQL_USER || 'lbrito',
+        user: process.env.MYSQL_USER || 'U3NJDpQoxN',
 
-            password: process.env.MYSQL_PASS || 'ThuReal.Dulc3',
+        password: process.env.MYSQL_PASS || '6NlXD034oS',
 
-            database: process.env.MYSQL_DB || 'movenu_db',
+        database: process.env.MYSQL_DB || 'U3NJDpQoxN',
 
-        }, */
+        },
 
-    ho: ocess.env.MYSQL_HOST || 'remotemysql.com',
+/*     ho: ocess.env.MYSQL_HOST || 'remotemysql.com',
 
     user: process.env.MYSQL_USER || 'U3NJDpQoxN',
 
     : process.env.MYSQL_PASS || '6NlXD034oS',
 
-    daba: process.env.MYSQL_DB || 'U3NJDpQoxN',
+    daba: process.env.MYSQL_DB || 'U3NJDpQoxN', */
 
-},
-},
-},
-/*     mysql: {
-                host: process.env.MYSQL_HOST || 'localhost',
-
-                user: process.env.MYSQL_USER || 'id11735746_lbrito',
-
-                password: process.env.MYSQL_PASS || 'balt0atena',
-
-                database: process.env.MYSQL_DB || 'id11735746_movenu_db',
-
-            }, */
-mysqlService: {
-        host: process.env.MYSQL_SRV_HOST || '127.0.0.1',
-        port: process.env.MYSQL_SRV_PORT || 3001,
-    },
+    /*     mysql: {
+                    host: process.env.MYSQL_HOST || 'localhost',
+    
+                    user: process.env.MYSQL_USER || 'id11735746_lbrito',
+    
+                    password: process.env.MYSQL_PASS || 'balt0atena',
+    
+                    database: process.env.MYSQL_DB || 'id11735746_movenu_db',
+    
+                }, */
+    mysqlService: {
+            host: process.env.MYSQL_SRV_HOST || '127.0.0.1',
+            port: process.env.MYSQL_SRV_PORT || 3002,
+        },
     cacheService: {
         host: process.env.MYSQL_SRV_HOST || '127.0.0.1',
         port: process.env.MYSQL_SRV_PORT || 3004,
     },
     redis: {
-        host: process.env.REDIS_HOST || 'redis-12187.c100.us-east-1-4.ec2.cloud.redislabs.com',
-        port: process.env.REDIS_PORT || '12187',
-        password: process.env.REDIS_PASS || 'Lbi8qLJrRtpfVWMBe3tn5h6rlhr418v8',
-    }
+        host: process.env.REDIS_HOST || 'redis-16916.c85.us-east-1-2.ec2.cloud.redislabs.com',
+        port: process.env.REDIS_PORT || '16916',
+        password: process.env.REDIS_PASS || ' alMzqCm8VetjEQsPITspXuSZBdv0GCsW',
+        }
 }
+
